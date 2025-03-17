9e59d9239552519cb002886705fec552aa4457d1
AlanKowalzky <alan.kowalzky@gmail.com>
2025-03-17 13:01:10 +0100

    init: task-6 no jest in one

--- import-service/tests/functions/importProductsFile.test.ts
+++ import-service/tests/functions/importProductsFile.test.ts
@@ -1,3 +1,5 @@
+import * as cdk from 'aws-cdk-lib';
+
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
